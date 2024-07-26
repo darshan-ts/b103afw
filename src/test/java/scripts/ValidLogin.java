29c4ead1ce7b988fa4fb2fc2b101c7420ae69c2e
darshan-ts <darshan.thippeswamy94@gmail.com>
2024-07-26 13:37:37 +0530

    Updated LoginPage with error message

--- src/test/java/scripts/ValidLogin.java
+++ src/test/java/scripts/ValidLogin.java
@@ -8,20 +8,28 @@ import generic.Utility;
 import page.HomePage;
 import page.LoginPage;
 
+
+
 public class ValidLogin extends BaseTest
 {
+
 	@Test(priority = 1)
 	public void testValidLogin()
 	{
+//		get data from excel
 		String un = Utility.getExcelData(EXCEL_PATH, "ValidLogin", 1, 0);
 		String pw = Utility.getExcelData(EXCEL_PATH, "ValidLogin", 1, 1);
+		
 //		1. enter valid user name
 		LoginPage loginPage=new LoginPage(driver);
 		loginPage.setUserName(un);
+		
 //		2. enter valid password
 		loginPage.setPassword(pw);
+		
 //		3. click on go button
 		loginPage.clickGoButton();
+		
 //		4. verify home page is displayed
 		HomePage homePage=new HomePage(driver);
 		boolean result = homePage.verifyHomePageIsDisplayed(wait);
